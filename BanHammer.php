d89b1de59a3c02fa09026190f292bc1a089a15a3
Comedyman937 <comedyman937@yahoo.com>
2014-03-14 07:10:35 -0400

    Major Bug Fixes
    - Fix for server crash
    - Fix for command errors
    - Fix for misspelled words
    - More alias
    - Separated functions
    - Faster working order

--- BanHammer.php
+++ BanHammer.php
@@ -4,7 +4,7 @@
 __PocketMine Plugin__
 name=BanHammer
 description=The BanHammer Has Spoken!
-version=1.0.0
+version=1.0.7
 author=Comedyman937
 class=BanHammer
 apiversion=11,12,13
@@ -25,18 +25,20 @@ class BanHammer implements Plugin
         {
             $this->CONFIG = new Config($this->api->plugin->configPath($this) . "config.yml", CONFIG_YAML, array(
                 "BanHammer" => 286,
-                "BanType" => add,
+                "BanType" => ban,
             ));
         }
 
         $this->CONFIG = $this->api->plugin->readYAML($this->api->plugin->configPath($this) . "config.yml");
 
         $this->api->console->register("banhammer", "Get the BanHammer!", array($this, "CommandHandler"));
-        $this->api->console->register("banhammeritem", "Change the BanHammer!", array($this, "CommandHandler"));
+        $this->api->console->register("banhammeritem", "Change the BanHammer Item!", array($this, "Item"));
+        $this->api->console->register("banhammertype", "Change the BanHammer Type!", array($this, "Type"));
 
         $this->api->addHandler("player.equipment.change", array($this, "EventHandler"));
         $this->api->addHandler("player.interact", array($this, "EventHandler"));
 
+        $this->api->console->alias("bht","banhammertype");
         $this->api->console->alias("bhi","banhammeritem");
         $this->api->console->alias("bh","banhammer");
 
@@ -48,11 +50,22 @@ class BanHammer implements Plugin
         switch(strtolower($cmd))
         {
             case "banhammer":
+                if(!($issuer instanceof Player)){
+                     return "[BanHammer] Please run this command in-game!";
+                }else{
+                     $this->api->console->run("give " . $issuer->username . " " . $this->CONFIG["BanHammer"] . " 1", "console", false);
 
-                $this->api->console->run("give " . $issuer->username . " " . $this->CONFIG["BanHammer"] . " 1", "console", false);
+                     return "[BanHammer] The BanHammer has been added to your inventory!\nInteract with a player to ban them!";
+                }
 
-                return "The BanHammer has been added to your inventory!\nTap on a player to ban them!";
+                break;
+          }
+     }
 
+     public function Item($cmd, $params, $issuer, $alias)
+     {
+        switch(strtolower($cmd))
+        {
             case "banhammeritem":
 
                 if(count($params) > 2)
@@ -73,29 +86,36 @@ class BanHammer implements Plugin
                 }
                 $this->api->plugin->writeYAML($this->api->plugin->configPath($this) . "config.yml", $this->CONFIG);
 
-                return "The BanHammer Item has been changed!";
-        }
+                return "[BanHammer] The BanHammer Item has been changed!";
+          }
+     }
 
+     public function Type($cmd, $params, $issuer, $alias)
+     {
+        switch(strtolower($cmd))
+        {
             case "banhammertype":
 
                 if(count($params) > 2)
                 {
-                    return "Usage: /banhammertype <ADD|IP>";
+                    return "Usage: /banhammertype <BAN|BANIP>";
                 }
-                if($params[0] = add || $params[0] = ip)
+                if($params[0] == "ban" || $params[0] == "banip")
                 {
-                                    if(isset($this->CONFIG["BanType"]))
-                                    {
-                                         $this->CONFIG["BanType"] = $params[0];
-                                    }
+                     if(isset($this->CONFIG["BanType"]))
+                     {
+                          $this->CONFIG["BanType"] = $params[0];
+                     }
                 }
                 else
                 {
-                    return "Usage: /banhammertype <ADD|IP>";
+                    return "Usage: /banhammertype <BAN|BANIP>";
                 }
                 $this->api->plugin->writeYAML($this->api->plugin->configPath($this) . "config.yml", $this->CONFIG);
 
-                return "The BanHammer Type has been changed!";
+                return "[BanHammer] The BanHammer Type has been changed!";
+
+                break;
         }
     }
 
@@ -122,7 +142,7 @@ class BanHammer implements Plugin
                 {
                     if(isset($this->TEMP[$data["entity"]->player->username]) and $this->TEMP[$data["entity"]->player->username] == true)
                     {                
-                        $this->api->console->run("ban " . $this->CONFIG["BanType"] . " " . $data["targetentity"]->player->username);
+                        $this->api->console->run($this->CONFIG["BanType"] . " add " . $data["targetentity"]->player->username);
                       
                         $this->api->chat->broadcast("[BanHammer] The BanHammer has spoken against " . $data["targetentity"]->player->username . "!");
                         
