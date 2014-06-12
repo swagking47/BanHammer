2d679c14bc7fcbe0c3a68d6556d04589711f266c
Philip Shilling <comedyman937@yahoo.com>
2014-06-12 10:24:15 -0400

    Added Commands

--- Plugin/New-API/BanHammer/src/BanHammer/Main.php
+++ Plugin/New-API/BanHammer/src/BanHammer/Main.php
@@ -3,16 +3,29 @@
 namespace BanHammer;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\Player;
 
 class Main extends PluginBase{
     public function onEnable(){
         $this->getLogger()->log("[BanHammer] BanHammer Loaded!");
     }
     
-    public function onCommand(){
-        
-    }
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+		switch($cmd->getName()){
+			case "banhammer":
+				if($args[0] == "edit"){
+				    //TODO BanHammer Editing
+				}elseif($args[0] == "get"){
+					//TODO Give BanHammer
+				}else{
+					$sender->sendMessage("Usage: /banhammer <edit|get>");
+				}
+			break;
+		}
+	}
     
    /**
     * @param PlayerInteractEvent $event
